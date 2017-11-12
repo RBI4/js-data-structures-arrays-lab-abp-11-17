bcc9082819e2ba8c19a73722f9a35dabcb755cd3
RBI4 <rebecca.inge@gmail.com>
2017-11-12 20:15:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveFirstDriver(){
   drivers.shift();
 }
 
-function appendDriver (name) {
+function appendDriver(name){
   return [...drivers, name];
 }
 
@@ -25,10 +25,10 @@ function prependDriver(name){
   return [name,...drivers];
 }
 
-function removeFirstDriver () {
+function removeFirstDriver(){
   return drivers.slice(1);
 }
 
 function removeLastDriver(){
-  return drivers.slice(0, drivers.length -1);
-})
+  return drivers.slice(0, drivers.length - 1);
+}
